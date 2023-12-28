d062f407d766a55688f91b4efdb12cf41c94b562
Mia <miabernhardt75@gmail.com>
2023-12-28 23:06:12 +0100

    added rendering of the level

--- src/GUI/TileCanvas.cpp
+++ src/GUI/TileCanvas.cpp
@@ -1,11 +1,16 @@
 #include "TileCanvas.hpp"
 
 #include <epoxy/gl.h>
+#include <wx/event.h>
 #include <wx/glcanvas.h>
+#include <wx/window.h>
 #include <wx/wx.h>
 
 #include "DX8Graphics.hpp"
+#include "ObjectList.hpp"
 #include "Tileengine.hpp"
+#include "Timer.hpp"
+
 BEGIN_EVENT_TABLE(TileCanvas, wxGLCanvas)
 EVT_PAINT(TileCanvas::PaintIt)
 END_EVENT_TABLE()
@@ -26,6 +31,9 @@ TileCanvas::TileCanvas(wxPanel* parent)
   Protokoll << "\n-> OpenGL init successful!\n" << std::endl;
 
   TileEngine.LoadSprites();
+
+  TileEngine.LoadLevel(g_storage_ext + "/data/levels/jungle.map");
+
   Bind(wxEVT_SIZE, [&](wxSizeEvent&){
     auto size = this->GetSize();
     DirectGraphics.ResizeToWindow(size.GetWidth(), size.GetHeight());
@@ -33,4 +41,35 @@ TileCanvas::TileCanvas(wxPanel* parent)
 
 }
 
-void TileCanvas::Render() {}
+void TileCanvas::Render() {
+  context->SetCurrent(*this);
+
+  Timer.update();
+
+  TileEngine.UpdateLevel();
+
+  DirectGraphics.ClearBackBuffer();
+
+  DirectGraphics.SetColorKeyMode();
+
+  TileEngine.CalcRenderRange();
+  TileEngine.DrawBackground();
+
+  TileEngine.DrawBackLevel();
+  TileEngine.DrawFrontLevel();
+
+  ObjectList.DrawAllObjects(TileEngine.XOffset, TileEngine.YOffset, TileEngine.Scale);
+
+  DirectGraphics.SetColorKeyMode();
+
+  TileEngine.DrawWater();
+  TileEngine.DrawBackLevelOverlay();
+  TileEngine.DrawOverlayLevel();
+  //TileEngine.DrawShadow();
+
+  DirectGraphics.DisplayBuffer();
+
+  
+  glFlush();
+  SwapBuffers();
+}
