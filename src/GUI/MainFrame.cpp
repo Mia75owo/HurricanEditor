79e4dba129aec15fe5ae3847081593985fc4e81b
Mia <miabernhardt75@gmail.com>
2023-12-28 23:09:30 +0100

    added open level file dialog

--- src/GUI/MainFrame.cpp
+++ src/GUI/MainFrame.cpp
@@ -7,11 +7,28 @@
 #include "Tileengine.hpp"
 
 enum IDs {
+  ID_LOAD = 2,
 };
 
 MainFrame::MainFrame(const wxString& title)
     : wxFrame(nullptr, wxID_ANY, title) {
+  auto menuFile = new wxMenu;
+  menuFile->Append(ID_LOAD, "&Load Map", "Opens a Hurrican map file");
+  menuFile->AppendSeparator();
+  menuFile->Append(wxID_EXIT);
 
+  auto menuBar = new wxMenuBar;
+  menuBar->Append(menuFile, "&File");
+  SetMenuBar(menuBar);
+
+  Bind(wxEVT_MENU, [&](wxCommandEvent&) {
+    wxFileDialog fileDialog(this, _("Open map File"), "", "",
+                       "map files (*.map)|*.map", wxFD_OPEN|wxFD_FILE_MUST_EXIST);
+    if (fileDialog.ShowModal() == wxID_CANCEL)
+      return;
+    Protokoll << fileDialog.GetPath().ToStdString() << std::endl;
+    TileEngine.LoadLevel(fileDialog.GetPath().ToStdString());
+  }, ID_LOAD);
   panel = new wxPanel(this);
 
   sizer = new wxBoxSizer(wxVERTICAL);
