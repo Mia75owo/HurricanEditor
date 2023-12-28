81c0711fa9c6a4d4f0e9f35b850003cc5d6eb3cc
Mia <miabernhardt75@gmail.com>
2023-12-28 23:09:38 +0100

    added save level file dialog

--- src/GUI/MainFrame.cpp
+++ src/GUI/MainFrame.cpp
@@ -8,12 +8,15 @@
 
 enum IDs {
   ID_LOAD = 2,
+  ID_SAVE = 3,
 };
 
 MainFrame::MainFrame(const wxString& title)
     : wxFrame(nullptr, wxID_ANY, title) {
   auto menuFile = new wxMenu;
   menuFile->Append(ID_LOAD, "&Load Map", "Opens a Hurrican map file");
+  menuFile->AppendSeparator();
+  menuFile->Append(ID_SAVE, "&Save Map", "Saves a Hurrican map file to a path");
   menuFile->AppendSeparator();
   menuFile->Append(wxID_EXIT);
 
@@ -29,6 +32,14 @@ MainFrame::MainFrame(const wxString& title)
     Protokoll << fileDialog.GetPath().ToStdString() << std::endl;
     TileEngine.LoadLevel(fileDialog.GetPath().ToStdString());
   }, ID_LOAD);
+  Bind(wxEVT_MENU, [&](wxCommandEvent&) {
+    wxFileDialog fileDialog(this, _("Save map File"), "", "",
+                       "map files (*.map)|*.map", wxFD_OPEN|wxFD_FILE_MUST_EXIST);
+    if (fileDialog.ShowModal() == wxID_CANCEL)
+      return;
+    TileEngine.SaveLevel(fileDialog.GetPath().ToStdString());
+  }, ID_SAVE);
+
   panel = new wxPanel(this);
 
   sizer = new wxBoxSizer(wxVERTICAL);
