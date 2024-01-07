6ad046b63ebce1b57bff5906825e3e94172966e8
Mia <miabernhardt75@gmail.com>
2024-01-07 15:38:53 +0100

    added check boxes for block flags

--- src/GUI/EditMenu.cpp
+++ src/GUI/EditMenu.cpp
@@ -1,10 +1,11 @@
 #include "EditMenu.hpp"
+
+#include <wx/wx.h>
+
 #include "GUI/App.hpp"
 #include "GUI/IDs.hpp"
 #include "GUI/TileSet.hpp"
 #include "Tileengine.hpp"
-
-#include <wx/wx.h>
 
 EditMenu::EditMenu(wxWindow* parent) : wxPanel(parent) {
   tileSet = new TileSet(this);
@@ -12,12 +13,43 @@ EditMenu::EditMenu(wxWindow* parent) : wxPanel(parent) {
 
   controls = new wxPanel(this);
 
-  setsChoice = new wxChoice(controls, ID_TILESET_CHOICE, wxDefaultPosition, wxDefaultSize, 0, NULL, wxCB_SORT);
+  setsChoice = new wxChoice(controls, ID_TILESET_CHOICE, wxDefaultPosition,
+                            wxDefaultSize, 0, NULL, wxCB_SORT);
   setsChoiceSizer = new wxBoxSizer(wxVERTICAL);
+
+  tileConfigBox = new wxStaticBox(controls, wxID_ANY, "Tile Config:");
+  tileConfigBoxSizer = new wxStaticBoxSizer(tileConfigBox, wxVERTICAL);
+  tileConfigSizer = new wxWrapSizer(wxHORIZONTAL, wxREMOVE_LEADING_SPACES);
+
+  // clang-format off
+  wall = new wxCheckBox(tileConfigBox, wxID_ANY, "wall", wxDefaultPosition, wxSize(100, 20));
+  enemyWall = new wxCheckBox(tileConfigBox, wxID_ANY, "enemy wall", wxDefaultPosition, wxSize(100, 20));
+  platform = new wxCheckBox(tileConfigBox, wxID_ANY, "platform", wxDefaultPosition, wxSize(100, 20));
+  light = new wxCheckBox(tileConfigBox, wxID_ANY, "light", wxDefaultPosition, wxSize(100, 20));
+  overlay = new wxCheckBox(tileConfigBox, wxID_ANY, "overlay", wxDefaultPosition, wxSize(100, 20));
+  animatedBack = new wxCheckBox(tileConfigBox, wxID_ANY, "anim back", wxDefaultPosition, wxSize(100, 20));
+  animatedFront = new wxCheckBox(tileConfigBox, wxID_ANY, "anim front", wxDefaultPosition, wxSize(100, 20));
+  water = new wxCheckBox(tileConfigBox, wxID_ANY, "water", wxDefaultPosition, wxSize(100, 20));
+  damage = new wxCheckBox(tileConfigBox, wxID_ANY, "damage", wxDefaultPosition, wxSize(100, 20));
+  conveyorL = new wxCheckBox(tileConfigBox, wxID_ANY, "conveyor L", wxDefaultPosition, wxSize(100, 20));
+  conveyorR = new wxCheckBox(tileConfigBox, wxID_ANY, "conveyor R", wxDefaultPosition, wxSize(100, 20));
+  turn = new wxCheckBox(tileConfigBox, wxID_ANY, "turn", wxDefaultPosition, wxSize(100, 20));
+  destroyable = new wxCheckBox(tileConfigBox, wxID_ANY, "destroyable", wxDefaultPosition, wxSize(100, 20));
+  moveL = new wxCheckBox(tileConfigBox, wxID_ANY, "move L", wxDefaultPosition, wxSize(100, 20));
+  overlayLight = new wxCheckBox(tileConfigBox, wxID_ANY, "overlay light", wxDefaultPosition, wxSize(100, 20));
+  swamp = new wxCheckBox(tileConfigBox, wxID_ANY, "swamp", wxDefaultPosition, wxSize(100, 20));
+  ice = new wxCheckBox(tileConfigBox, wxID_ANY, "ice", wxDefaultPosition, wxSize(100, 20));
+  moveD = new wxCheckBox(tileConfigBox, wxID_ANY, "move D", wxDefaultPosition, wxSize(100, 20));
+  waterfall = new wxCheckBox(tileConfigBox, wxID_ANY, "waterfall", wxDefaultPosition, wxSize(100, 20));
+  moveR = new wxCheckBox(tileConfigBox, wxID_ANY, "move R", wxDefaultPosition, wxSize(100, 20));
+  slopeL = new wxCheckBox(tileConfigBox, wxID_ANY, "slope L", wxDefaultPosition, wxSize(100, 20));
+  slopeR = new wxCheckBox(tileConfigBox, wxID_ANY, "slope R", wxDefaultPosition, wxSize(100, 20));
+  liquid = new wxCheckBox(tileConfigBox, wxID_ANY, "liquid", wxDefaultPosition, wxSize(100, 20));
+  // clang-format on
 
   tileInfoBox = new wxStaticBox(controls, wxID_ANY, "Tile Info:");
   tileInfoBoxSizer = new wxStaticBoxSizer(tileInfoBox, wxVERTICAL);
-  positionText = new wxStaticText(tileInfoBox, wxID_ANY, "");;
+  positionText = new wxStaticText(tileInfoBox, wxID_ANY, "");
 }
 
 void EditMenu::Init() {
@@ -26,27 +58,56 @@ void EditMenu::Init() {
 
   for (auto& loadedTileSet : TileEngine.LoadedTilesetPathsWithID) {
     // TODO: make non hardcoded
-    tileSet->LoadTileSet(wxString::Format("../data/textures/%s", loadedTileSet.first), loadedTileSet.second, wxBITMAP_TYPE_PNG);
+    tileSet->LoadTileSet(
+        wxString::Format("../data/textures/%s", loadedTileSet.first),
+        loadedTileSet.second, wxBITMAP_TYPE_PNG);
     setsChoice->Append(loadedTileSet.first);
   }
   setsChoice->Select(0);
 
-  auto font = wxFont(18, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL);
+  auto font =
+      wxFont(18, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL);
   positionText->SetFont(font);
+
+  tileConfigSizer->Add(wall, 0, wxEXPAND);
+  tileConfigSizer->Add(enemyWall, 0, wxEXPAND);
+  tileConfigSizer->Add(platform, 0, wxEXPAND);
+  tileConfigSizer->Add(light, 0, wxEXPAND);
+  tileConfigSizer->Add(overlay, 0, wxEXPAND);
+  tileConfigSizer->Add(animatedBack, 0, wxEXPAND);
+  tileConfigSizer->Add(animatedFront, 0, wxEXPAND);
+  tileConfigSizer->Add(water, 0, wxEXPAND);
+  tileConfigSizer->Add(damage, 0, wxEXPAND);
+  tileConfigSizer->Add(conveyorL, 0, wxEXPAND);
+  tileConfigSizer->Add(conveyorR, 0, wxEXPAND);
+  tileConfigSizer->Add(turn, 0, wxEXPAND);
+  tileConfigSizer->Add(destroyable, 0, wxEXPAND);
+  tileConfigSizer->Add(moveL, 0, wxEXPAND);
+  tileConfigSizer->Add(overlayLight, 0, wxEXPAND);
+  tileConfigSizer->Add(swamp, 0, wxEXPAND);
+  tileConfigSizer->Add(ice, 0, wxEXPAND);
+  tileConfigSizer->Add(moveD, 0, wxEXPAND);
+  tileConfigSizer->Add(waterfall, 0, wxEXPAND);
+  tileConfigSizer->Add(moveR, 0, wxEXPAND);
+  tileConfigSizer->Add(slopeL, 0, wxEXPAND);
+  tileConfigSizer->Add(slopeR, 0, wxEXPAND);
+  tileConfigSizer->Add(liquid, 0, wxEXPAND);
+
+  tileConfigBoxSizer->Add(tileConfigSizer, 0, wxEXPAND);
 
   tileInfoBox->SetFont(font);
   tileInfoBoxSizer->Add(positionText, 0, wxEXPAND);
 
   setsChoiceSizer->Add(setsChoice, 0, wxEXPAND);
   setsChoiceSizer->Add(tileInfoBoxSizer, 0, wxEXPAND, 10);
+  setsChoiceSizer->Add(tileConfigBoxSizer, 0, wxEXPAND, 10);
   controls->SetSizerAndFit(setsChoiceSizer);
 
-  sizer->Add(tileSet, 0, wxSHAPED|wxEXPAND);
+  sizer->Add(tileSet, 0, wxSHAPED | wxEXPAND);
   sizer->Add(controls, 1, wxEXPAND);
 
   sizer->SetSizeHints(this);
   this->SetSizerAndFit(sizer);
-
 
   // =====
   // BINDS
--- src/GUI/EditMenu.hpp
+++ src/GUI/EditMenu.hpp
@@ -1,19 +1,51 @@
 #ifndef EDIT_MENU_HPP_
 #define EDIT_MENU_HPP_
 
+#include <wx/wrapsizer.h>
 #include <wx/wx.h>
+
+#include <cstdint>
+
 #include "GUI/TileSet.hpp"
 
 class EditMenu : public wxPanel {
-public:
+ public:
   EditMenu(wxWindow* parent);
   void Init();
 
   TileSet* tileSet;
-private:
 
+ private:
   wxPanel* controls;
   wxChoice* setsChoice;
+
+  wxStaticBox* tileConfigBox;
+  wxStaticBoxSizer* tileConfigBoxSizer;
+  wxWrapSizer* tileConfigSizer;
+
+  wxCheckBox* wall;
+  wxCheckBox* enemyWall;
+  wxCheckBox* platform;
+  wxCheckBox* light;
+  wxCheckBox* overlay;
+  wxCheckBox* animatedBack;
+  wxCheckBox* animatedFront;
+  wxCheckBox* water;
+  wxCheckBox* damage;
+  wxCheckBox* conveyorL;
+  wxCheckBox* conveyorR;
+  wxCheckBox* turn;
+  wxCheckBox* destroyable;
+  wxCheckBox* moveL;
+  wxCheckBox* overlayLight;
+  wxCheckBox* swamp;
+  wxCheckBox* ice;
+  wxCheckBox* moveD;
+  wxCheckBox* waterfall;
+  wxCheckBox* moveR;
+  wxCheckBox* slopeL;
+  wxCheckBox* slopeR;
+  wxCheckBox* liquid;
 
   wxStaticBox* tileInfoBox;
   wxStaticText* positionText;
